907130f01967a6a2ee31c7e4b11720d63e60d0c6
Flake212 <111091522+Flake2121@users.noreply.github.com>
2024-05-15 13:03:33 +0100

    Update 3xplusone.py

--- 3xplusone.py
+++ 3xplusone.py
@@ -1,3 +1,2 @@
-x=int(input("n: "));i=0
-while x!=1:x+=(x/-2,(x*2)+1)[int(x%2)];i+=1;print(x);
-print("i:",i);input()
+x=int(input("n: "));i=0;
+while x!=1:x+=(x/-2,(x*2)+1)[int(x%2)];i+=1;print(x,"i:",i+1)
